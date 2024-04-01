6b3f571f1ae8ae209d5117d439231a86fceec423
ShockDexterity <canterw8353@my.uwstout.edu>
2024-04-01 00:18:55 -0500

    lots of styling done

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function Header ({ title, subtitle }) {
   return (
     <div className="row">
-      <div className="border-y-2 text-center py-2">
+      <div className="border-y-2 border-t-0 text-center py-2">
         <h1 className="text-2xl oxygen-bold">{title}</h1>
         <p className="text-sm oxygen-light">
           <i>{subtitle}</i>
--- client/components/PlanetCard.jsx
+++ client/components/PlanetCard.jsx
@@ -10,35 +10,25 @@ export default function PlanetCard ({
     extreme, // boolean
     infested, // boolean
     special, // string
-    resources, // string[]
+    resources, // object with 3 strings
     sentinels, // string
     system // string
   }
 }) {
-  let biomeBG = ''
-  if (exotic) {
-    biomeBG = 'bg-slate-400'
-  }
-
-  const sentinelBG =
-    sentinels === 'low'
-      ? 'bg-green-500'
-      : sentinels === 'high'
-        ? 'bg-yellow-500'
-        : sentinels === 'aggressive'
-          ? 'bg-red-500'
-          : 'bg-purple-400'
-
   const sentinelText = <p>Sentinel Level: {sentinels.toUpperCase()}</p>
 
   return (
-    <div className="bg-gray-200 p-4 oxygen-regular border-gray-500 border-2 rounded-lg">
-      <h2>{name}</h2>
-      <p className={biomeBG}>
-        {descriptor} ({biome})
-      </p>
-      <p className={sentinelBG}>{sentinels !== 'low' && sentinelText}</p>
-      <p>{system} System</p>
+    <div className="oxygen-regular rounded-lg border-2 border-gray-500 bg-gray-200 p-4">
+      <div className="border-y-2 border-t-0 border-black">{name}</div>
+      <div className="pt-2">
+        <p className={getBiomeBackground(exotic, extreme, infested)}>
+          {descriptor} {/* ({biome}) */}
+        </p>
+      </div>
+      <div className={getSentinelBackground(sentinels)}>
+        {sentinels !== 'low' && sentinelText}
+      </div>
+      <div>{system} System</div>
     </div>
   )
 }
@@ -48,57 +38,49 @@ PlanetCard.propTypes = {
   planet: PropTypes.shape({
     name: PropTypes.string.isRequired,
     descriptor: PropTypes.string.isRequired,
-    biome: PropTypes.string.isRequired,
+    biome: PropTypes.string,
     exotic: PropTypes.bool.isRequired,
     extreme: PropTypes.bool.isRequired,
     infested: PropTypes.bool.isRequired,
-    special: PropTypes.string.isRequired,
-    resources: PropTypes.arrayOf(PropTypes.string).isRequired,
+    special: PropTypes.string,
+    resources: PropTypes.exact({
+      r1: PropTypes.string.isRequired,
+      r2: PropTypes.string.isRequired,
+      r3: PropTypes.string.isRequired
+    }),
     sentinels: PropTypes.string.isRequired,
     system: PropTypes.string.isRequired
   }).isRequired
 }
 
-/*
-const card = document.createElement('div')
-card.className = 'card'
-
-const planetName = document.createElement('span')
-planetName.className = 'planetInfo planetName'
-planetName.textContent = planet.name
-card.appendChild(planetName)
-
-const planetBiome = document.createElement('div')
-planetBiome.className = 'planetInfo'
-if (planet.exotic) {
-  planetBiome.className += ' exotic'
+function getBiomeBackground (exotic, extreme, infested) {
+  if (exotic && extreme) {
+    return 'bg-gradient-to-r from-cyan-500 to-red-500'
+  }
+  if (extreme && infested) {
+    return 'bg-gradient-to-r from-red-400 to-green-500'
+  }
+  if (exotic) {
+    return 'bg-cyan-500'
+  }
+  if (extreme) {
+    return 'bg-red-400'
+  }
+  if (infested) {
+    return 'bg-green-500'
+  }
+  return 'bg-inherit'
 }
-if (planet.extreme) {
-  planetBiome.className += ' extreme'
-}
-if (planet.infested) {
-  planetBiome.className += ' infested'
-}
-planetBiome.textContent = `${planet.biome} Planet`
-card.appendChild(planetBiome)
 
-const sentinelLevel = document.createElement('div')
-sentinelLevel.className = 'planetInfo'
-if (planet.sentinels === 'Aggressive') {
-  sentinelLevel.className += ' aggressive'
-  console.log(sentinelLevel.classList)
+function getSentinelBackground (sentinel) {
+  if (sentinel === 'low') {
+    return 'bg-green-500'
+  }
+  if (sentinel === 'high') {
+    return 'bg-yellow-500'
+  }
+  if (sentinel === 'aggressive') {
+    return 'bg-red-500'
+  }
+  return 'bg-purple-400'
 }
-else if (planet.sentinels === 'Corrupt') {
-  sentinelLevel.className += ' corrupt'
-}
-sentinelLevel.textContent = `${planet.sentinels} Sentinels`
-card.appendChild(sentinelLevel)
-
-const planetSystem = document.createElement('div')
-planetSystem.className = 'planetInfo'
-planetSystem.textContent = `${planet.system} System`
-card.appendChild(planetSystem)
-
-cardContainer.appendChild(card)
-planetRow.appendChild(cardContainer)
-*/
